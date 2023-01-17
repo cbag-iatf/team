c221bfdf294a355a6107e36d11ba551bf3b9a5a0
cbag-iatf <c.goudiaby@iatf.it>
2023-01-17 11:07:01 +0000

    wip

---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,42 @@
+<aside class="aside is-placed-left is-expanded">
+    <div class="aside-tools">
+        <div>
+            Admin <b class="font-black">One</b>
+        </div>
+    </div>
+    <div class="menu is-menu-main">
+        <p class="menu-label">General</p>
+        <ul class="menu-list">
+            <li class="active">
+                <a href="index.html">
+                    <span class="icon"><i class="mdi mdi-desktop-mac"></i></span>
+                    <span class="menu-item-label">Dashboard</span>
+                </a>
+            </li>
+        </ul>
+        <p class="menu-label">Parametres</p>
+        <ul class="menu-list">
+      
+            <li>
+                <a class="dropdown">
+                    <span class="icon"><i class="mdi mdi-view-list"></i></span>
+                    <span class="menu-item-label">Configuration</span>
+                    <span class="icon"><i class="mdi mdi-chevron-down"></i></span>
+                </a>
+                <ul>
+                    <li>
+                        <a href="#void">
+                            <span>User</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#void">
+                            <span>Roles and permissions</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    
+    </div>
+</aside>
